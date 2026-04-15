e8265089805972616f613312d98205b1575f2bce
Wathisa <wathisa@gmail.com>
2026-04-15 12:17:04 +0700

    add JS fetch 5 files

---
+++ javascript/promises/fetch_01.js
@@ -0,0 +1,17 @@
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+fetch(url);
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    return data;
+  })
+  // handle error gracefully
+  .catch((error) => {
+    console.error("Something went wrong!", error);
+    return error;
+  });
