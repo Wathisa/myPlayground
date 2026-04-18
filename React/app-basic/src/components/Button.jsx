2534e264726e874a606448410070dfea3fcf89a3
Wathisa <wathisa@gmail.com>
2026-04-18 15:42:31 +0700

    react components

---
+++ React/app-basic/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ text }) => {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
---
+++ React/app-basic/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button";
+
+const Home = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Welcome to HomePage</h1>
+      <Button text="Click me!" />
+    </div>
+  );
+};
+
+export default Home;
